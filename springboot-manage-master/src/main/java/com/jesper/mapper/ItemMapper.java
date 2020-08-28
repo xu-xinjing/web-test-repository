f0bf4c32be2c540570b13eb4f4908d9a3e74d71b
xu-xinjing <1403548876@qq.com>
2020-08-28 13:31:25 +0800

    add item manage module

---
+++ springboot-manage-master/src/main/java/com/jesper/mapper/ItemMapper.java
@@ -0,0 +1,27 @@
+package com.jesper.mapper;
+
+import com.jesper.model.Item;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ItemMapper {
+
+    Item findById(Item item);
+
+    void delete(Item item);
+
+    List<Item> list(Item item);
+
+    List<Item> listS(Item item);
+
+    int count(Item item);
+
+    int insert(Item item);
+
+    int update (Item item);
+
+
+    List<Item> selectAll();
+}
